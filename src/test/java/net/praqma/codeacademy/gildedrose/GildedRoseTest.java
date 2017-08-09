d1d97c365cc17b055cf86e89c666b74072ac8a3e
Yanislav Zagorov <yanislavzagorov@gmail.com>
2017-08-09 15:05:10 +0000

    Commented out test

--- src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
+++ src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
@@ -14,7 +14,7 @@ public class GildedRoseTest {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+        assertEquals("failed", app.items[0].name);
     }
 
 }
